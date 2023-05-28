43d5ff15e5fac125dee50f3e71302ac84f97dfbc
QA-Lexx <ilin_001@mail.ru>
2023-05-28 17:38:15 +0300

    change code in file called task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,53 +5,49 @@
 function solveEquation(a, b, c) {
 
 	let arrSolveEquation = [];
-
 	let discriminant = b ** 2 - (4 * a * c);
-	Math.sqrt(discriminant);
-	console.log(discriminant);
 	let i;
+	let o;
 
 	if (discriminant < 0) {
-		console.log(arrSolveEquation);
+		let emptyArr = [];
 	} else if (discriminant = 0) {
 		i = -b / (2 * a);
 		arrSolveEquation.push(i);
-		console.log(arrSolveEquation);
-	} else(discriminant > 0);
-	i = (-b + Math.sqrt(discriminant)) / (2 * a) && (-b - Math.sqrt(discriminant)) / (2 * a);
-	for (i = 0; i < arrSolveEquation.length; i++) {
+	} else {
+		i = (-b + Math.sqrt(discriminant)) / (2 * a);
+		o = (-b - Math.sqrt(discriminant)) / (2 * a);
 		arrSolveEquation.push(i);
-		console.log(arrSolveEquation);
+		arrSolveEquation.push(o);
 	}
 
 	return arrSolveEquation;
 
 }
 
-console.log(solveEquation(1, 2, 3));
-
 // Задача 2
 
 "use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-
-	console.log(percent, typeof percent, !Number.isNaN(percent));
+	Number(percent);
+	Number(contribution);
+	Number(amount);
+	Number(countMonths);
 
 	if (Number.isNaN(percent) && Number.isNaN(contribution) && Number.isNaN(amount)) {
-		const perMonth = percent / 100 / 12;
-		console.log('perMonth', perMonth);
+		Number(perMonth);
+		Number(bodyCredit);
+		Number(monthly);
+		Number(totalSum);
 
+		const perMonth = percent / 100 / 12;
 		const bodyCredit = amount - contribution;
 		const monthly = bodyCredit * (perMonth + (perMonth / (((1 + perMonth) ** countMonths) - 1)));
 		const totalSum = parseFloat((monthly * countMonths).toFixed(2));
 		return totalSum;
 	}
 
-	return false;
+	return calculateTotalMortgage;
 
 }
-
-console.log(calculateTotalMortgage("test", 0, 10000, 36));
-console.log(calculateTotalMortgage("0", 0, 10000, 36));
-console.log(calculateTotalMortgage("3", 0, 10000, 36));
