281caa476ce9859446e7cb75178b3e76cf110b46
QA-Lexx <ilin_001@mail.ru>
2023-06-09 19:31:43 +0300

    create code in task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+
+    this.setSubject = function (subjectName) {
+        this.subject = subjectName;
+    };
+
+    this.addMarks = function (...marksToAdd) {
+        if (!this.marks) {
+            console.log("Error: Student has been expelled.");
+            return;
+        }
+        this.marks.push(...marksToAdd);
+    };
+
+    this.getAverage = function () {
+        if (!this.marks || !this.marks.length) {
+            return 0;
+        }
+        const sum = this.marks.reduce((acc, curr) => acc + curr);
+        return sum / this.marks.length;
+    };
+
+    this.exclude = function (reason) {
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    };
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage());
+console.log(student1);
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+let student2 = new Student("Артем", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude("Плохая учеба");
+console.log(student2);
